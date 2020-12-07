d0987bd7a2e166e0a484b0b1137bd584f795a6f1
anikachristopher <anika.mchristoper@gmail.com>
2020-12-06 19:14:41 -0500

    updated seed data

--- afterschool_backend/config/routes.rb
+++ afterschool_backend/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :children
-  resources :schedules
+  resources :schedules, only: [:index, :show, :update, :create, :destroy]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-end
 
 
