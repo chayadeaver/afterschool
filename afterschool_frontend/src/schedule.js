afc35cde3244e3b2f41b54e01446a4ce6b12ee34
anikachristopher <anika.mchristoper@gmail.com>
2020-12-10 19:21:17 -0500

    set up schedule class

---
+++ afterschool_frontend/src/schedule.js
@@ -0,0 +1,9 @@
+class Schedule{
+    constructor(id, weekday, subject, content){
+        this.id = id
+        this.weekday = weekday
+        this.subject = subject
+        this.content = content
+    }
+
+}
