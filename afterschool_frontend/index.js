864d60b18c25b1e1f1b93c528704eef13452617c
anikachristopher <anika.mchristoper@gmail.com>
2020-12-10 23:13:11 -0500

    fetched data in json format

--- afterschool_frontend/index.js
+++ afterschool_frontend/index.js
@@ -2,14 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchSchedules()
 })
 
-// const BASE_URL = "http://127.0.0.1:3000"
+const BASE_URL = "http://127.0.0.1:3000"
 
-// function fetchSchedules(){
-//     fetch(`${BASE_URL}/schedules}`)
-//     .then(resp => resp.json())
-//     .then(schedules =>{
+//read - index
+function fetchSchedules(){
+    fetch(`${BASE_URL}/schedules`)
+    .then(resp => resp.json())
+    .then(schedules => {})
+    }
 
-//     })
-// }
 
-// <div id="schedules-container"></div>
+//create
+//update
