cd7e7f99be47d8591c147a45d9b4639171cc831c
anikachristopher <anika.mchristoper@gmail.com>
2020-12-10 19:16:22 -0500

    add event listener and connected html files

--- afterschool_frontend/index.js
+++ afterschool_frontend/index.js
@@ -1,4 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { 
     fetchSchedules()
-}
-)
+})
+
+// const BASE_URL = "http://127.0.0.1:3000"
+
+// function fetchSchedules(){
+//     fetch(`${BASE_URL}/schedules}`)
+//     .then(resp => resp.json())
+//     .then(schedules =>{
+
+//     })
+// }
+
+// <div id="schedules-container"></div>
