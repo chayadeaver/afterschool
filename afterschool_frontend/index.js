18b04c3c24c78bea450c0881ef5704b0681788a3
anikachristopher <anika.mchristoper@gmail.com>
2020-12-10 23:37:42 -0500

    create js objects

--- afterschool_frontend/index.js
+++ afterschool_frontend/index.js
@@ -8,7 +8,13 @@ const BASE_URL = "http://127.0.0.1:3000"
 function fetchSchedules(){
     fetch(`${BASE_URL}/schedules`)
     .then(resp => resp.json())
-    .then(schedules => {})
+    .then(schedules => {
+        for (const schedule of schedules){
+        
+            let s = new Schedule(schedule.id, schedule.weekday, schedule.subject, schedule.content)
+            
+        }
+    })
     }
 
 
