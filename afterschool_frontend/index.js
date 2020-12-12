6162a8ba779e59581eeb6f04421a68fcfee25be2
anikachristopher <anika.mchristoper@gmail.com>
2020-12-12 15:24:41 -0500

    add function to fetch from the schedules endpoint

--- afterschool_frontend/index.js
+++ afterschool_frontend/index.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => { 
     fetchSchedules()
+
+    const createScheduleForm = document.querySelector('#new-schedule-form');
+    createScheduleForm.addEventListener("submit", (e) => createScheduleFormHandler(e));
 })
 
 const BASE_URL = "http://127.0.0.1:3000"
@@ -8,14 +11,29 @@ const BASE_URL = "http://127.0.0.1:3000"
 function fetchSchedules(){
     fetch(`${BASE_URL}/schedules`)
     .then(resp => resp.json())
-    .then(schedules => {
-        for (const schedule of schedules){
+    .then(json => renderSchedules(json))
+    
+    // {
+    //     for (const schedule of schedules){
         
-            let s = new Schedule(schedule.id, schedule.weekday, schedule.subject, schedule.content)
-            
-        }
-    })
+    //         let s = new Schedule(schedule.id, schedule.weekday, schedule.subject, schedule.content)
+    //         //for each schedule in this array we are creating a new schedule passing in the attribute listed
+    //         document.querySelector('#schedules-container'.innerHTML += new Schedule)
+    //     }
     }
+
+    // function renderSchedules(schedules) {
+    //     const schedulesDiv = document.querySelector('schedules-container')
+    //     schedules.forEach(schedule => {
+    //       const h2 = document.createElement('h2')
+    //       h2.innerHTML = schedule.weekday
+    //       schedulesDiv.appendChild(h2)
+    //     })
+    //   }
+
+
+    // function renderSchedules
+    
 
 
 //create
--- afterschool_frontend/src/schedule.js
+++ afterschool_frontend/src/schedule.js
@@ -4,6 +4,43 @@ class Schedule{
         this.weekday = weekday
         this.subject = subject
         this.content = content
+        // creating and initializing objects of this class
     }
 
+    // renderSchedules() {
+
+    //     let schedulesDiv = document.getElementById("schedules-container")
+    //     //will collect any current and future schedules
+
+    renderSchedules() {
+        return (
+            `<div id=${this.id}>
+                <h4>Day: ${this.weekday}</h4>
+                <h4>Subject: ${this.subject}</h4>
+                <h4>Content: ${this.content}</h4>
+                <br>     
+                <button onclick="delSched(${this.id})">Delete Schedule</button>
+            </div>
+            <hr>`
+        )
+    }
 }
+
+Schedule.all = [];
+
+
+
+
+
+        // const newSchedule = document.createElement("div")
+        // newSchedule.innerHTML;
+        // div.appendChild(newSchedule)
+        // newSchedule.textContent = "Weekday", "Subject", "Content"
+
+        // const scheduleDiv = document.createElement("div");
+        // const newContent = document.createTextNode("weekday")
+        // scheduleDiv.appendChild(newContent);
+        // const currentDiv = document.getElementById("div"); 
+        // document.body.insertBefore(schedulesDiv, currentDiv); 
+        
+    
