988e28809c9403c9534fb7fbfb1c4c5079db187e
anikachristopher <anika.mchristoper@gmail.com>
2020-12-13 13:38:26 -0500

    appended the rendered schedule to the DOM

--- afterschool_frontend/src/schedule.js
+++ afterschool_frontend/src/schedule.js
@@ -13,20 +13,39 @@ class Schedule{
     //     //will collect any current and future schedules
 
     renderSchedule() {
-        return (
-            `<div id=${this.id}>
-                <h4>Day: ${this.weekday}</h4>
-                <h4>Subject: ${this.subject}</h4>
-                <h4>Content: ${this.content}</h4>
-                <br>     
-                <button onclick="delSched(${this.id})">Delete Schedule</button>
-            </div>
-            <hr>`
-        )
+
+        const col = document.querySelector('tr');
+        const newSchedule = document.createElement('th');
+        document.body.appendChild(newSchedule)
+        newSchedule.textContent = "Test"
+        
+
+
+        //  schedulesDiv = document.getElementById("schedules-container")
+
+        // schedulesDiv.innerHTML +=
+
+            //  <ul>
+            //  <h4>Day: ${this.weekday}</h4>
+            //  <li>Subject: ${this.subject}</li>
+            //  <li>Content: ${this.content}</li>
+            //  </ul>
+        
+        
+        // return (
+            // `<div id=${this.id}>
+            //     <h4>Day: ${this.weekday}</h4>
+            //     <h4>Subject: ${this.subject}</h4>
+            //     <h4>Content: ${this.content}</h4>
+            //     <br>     
+            //     <button onclick="delSched(${this.id})">Delete Schedule</button>
+            // </div>
+            // <hr>`
+        
     }
 }
 
-Schedule.all = [];
+// Schedule.all = [];
 
 
 
