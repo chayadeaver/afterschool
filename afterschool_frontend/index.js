ba48335a9fefd1c6e8ddef5d53e387be4de37391
anikachristopher <anika.mchristoper@gmail.com>
2020-12-12 23:12:20 -0500

    created js objects that mimic rails objects

--- afterschool_frontend/index.js
+++ afterschool_frontend/index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => { 
     fetchSchedules()
 
-    const createScheduleForm = document.querySelector('#new-schedule-form');
-    createScheduleForm.addEventListener("submit", (e) => createScheduleFormHandler(e));
+    // const createScheduleForm = document.querySelector('#new-schedule-form');
+    // createScheduleForm.addEventListener("submit", (e) => createScheduleFormHandler(e));
 })
 
 const BASE_URL = "http://127.0.0.1:3000"
@@ -11,12 +11,21 @@ const BASE_URL = "http://127.0.0.1:3000"
 function fetchSchedules(){
     fetch(`${BASE_URL}/schedules`)
     .then(resp => resp.json())
-    .then(json => renderSchedules(json))
+    .then(schedules => {
     
-    // {
-    //     for (const schedule of schedules){
+    for (const schedule of schedules){
+        //for each schedule, create a new schedule object with the following properties.
+        let s = new Schedule(schedule.id, schedule.weekday, schedule.subject, schedule.content)
+       
+    ;
+    }
+})
+    
+
+// }
+//         for (const schedule of schedules){
         
-    //         let s = new Schedule(schedule.id, schedule.weekday, schedule.subject, schedule.content)
+//             let s = new Schedule(schedule.id, schedule.weekday, schedule.subject, schedule.content)
     //         //for each schedule in this array we are creating a new schedule passing in the attribute listed
     //         document.querySelector('#schedules-container'.innerHTML += new Schedule)
     //     }
@@ -37,4 +46,4 @@ function fetchSchedules(){
 
 
 //create
-//update
+//delete
