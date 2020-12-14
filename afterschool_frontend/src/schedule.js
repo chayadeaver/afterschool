fea52eac0f68cb0c7915c8e3d415a79e75fea72a
anikachristopher <anika.mchristoper@gmail.com>
2020-12-13 19:05:13 -0500

    update render schedule method

--- afterschool_frontend/src/schedule.js
+++ afterschool_frontend/src/schedule.js
@@ -1,5 +1,5 @@
 class Schedule{
-    constructor(id, weekday, subject, content){
+    constructor(id, weekday, subject, content, week){
         this.id = id
         this.weekday = weekday
         this.subject = subject
@@ -8,59 +8,18 @@ class Schedule{
         // creating and initializing objects of this class
     }
 
-    // renderSchedules() {
-
-    //     let schedulesDiv = document.getElementById("schedules-container")
-    //     //will collect any current and future schedules
-
-    renderSchedule() {
-
-        const col = document.querySelector('tr');
-        const newSchedule = document.createElement('th');
-        document.body.appendChild(newSchedule)
-        newSchedule.textContent = "Test"
+        renderSchedule() {
+            //grabbing the math table
+            let mathArea = document.querySelector("math-table"); 
+            mathArea.classList.add("table")
+            mathArea.classList.add("table-success")
+            mathArea.classList.add("table-striped")
         
-
-
-        //  schedulesDiv = document.getElementById("schedules-container")
-
-        // schedulesDiv.innerHTML +=
-
-            //  <ul>
-            //  <h4>Day: ${this.weekday}</h4>
-            //  <li>Subject: ${this.subject}</li>
-            //  <li>Content: ${this.content}</li>
-            //  </ul>
-        
-        
-        // return (
-            // `<div id=${this.id}>
-            //     <h4>Day: ${this.weekday}</h4>
-            //     <h4>Subject: ${this.subject}</h4>
-            //     <h4>Content: ${this.content}</h4>
-            //     <br>     
-            //     <button onclick="delSched(${this.id})">Delete Schedule</button>
-            // </div>
-            // <hr>`
-        
-    }
+            //creating a new table row
+            let mathRow = document.createElement('th');
+           
+        }
 }
 
-// Schedule.all = [];
 
-
-
-
-
-        // const newSchedule = document.createElement("div")
-        // newSchedule.innerHTML;
-        // div.appendChild(newSchedule)
-        // newSchedule.textContent = "Weekday", "Subject", "Content"
-
-        // const scheduleDiv = document.createElement("div");
-        // const newContent = document.createTextNode("weekday")
-        // scheduleDiv.appendChild(newContent);
-        // const currentDiv = document.getElementById("div"); 
-        // document.body.insertBefore(schedulesDiv, currentDiv); 
-        
     
