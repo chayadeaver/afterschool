8012df6ddabaecda859d1b1471c3c2b5cd217b7b
anikachristopher <anika.mchristoper@gmail.com>
2020-12-19 13:09:49 -0500

    updated render method

--- afterschool_frontend/src/schedule.js
+++ afterschool_frontend/src/schedule.js
@@ -11,17 +11,28 @@ class Schedule{
     renderSchedule() {
     //grabbing the math table
         let mathArea = document.getElementById("math-table"); 
+        let readingArea = document.getElementById("reading-table"); 
+        let creativeArea = document.getElementById("creative-table"); 
+        let physicalEdArea = document.getElementById("physicale-table"); 
+        let musicArea = document.getElementById("music-table"); 
             
         //creating a new table row
         let row = mathArea.insertRow(0);
+        let row1 = readingArea.insertRow(0);
+        let row2 = creativeArea.insertRow(0);
+        let row3 = physicalEdArea.insertRow(0);
+        let row4 = musicArea.insertRow(0);
 
         //inserting cells into the rows
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
 
         //add content to cell
         cell1.innerHTML = "Test Cell 1";
         cell2.innerHTML = "Test Cell 2";
+        cell3.innerHTML = "Test Cell 3";
+    
         // let mathRow = document.createElement('tr');
         // mathContent = document.innertext("");
         // mathContent = document.getElementById("class");
