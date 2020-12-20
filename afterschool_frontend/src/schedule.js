ccc8306e223e1d9521360647dc4ed6bc9143bf35
anikachristopher <anika.mchristoper@gmail.com>
2020-12-19 20:19:21 -0500

    built the render schedule method

--- afterschool_frontend/src/schedule.js
+++ afterschool_frontend/src/schedule.js
@@ -8,42 +8,39 @@ class Schedule{
         // creating and initializing objects of this class
     }
 
+
     renderSchedule() {
     //grabbing the math table
-        let mathArea = document.getElementById("math-table"); 
-        let readingArea = document.getElementById("reading-table"); 
-        let creativeArea = document.getElementById("creative-table"); 
-        let physicalEdArea = document.getElementById("physicale-table"); 
-        let musicArea = document.getElementById("music-table"); 
-            
-        //creating a new table row
-        let row = mathArea.insertRow(0);
-        let row1 = readingArea.insertRow(0);
-        let row2 = creativeArea.insertRow(0);
-        let row3 = physicalEdArea.insertRow(0);
-        let row4 = musicArea.insertRow(0);
+        let mondayArea = document.getElementById("monday-table"); 
+        let tuesdayArea = document.getElementById("tuesday-table"); 
+        let wednesdayArea = document.getElementById("wednesday-table"); 
 
-        //inserting cells into the rows
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        let cell3 = row.insertCell(2);
-
-        //add content to cell
-        cell1.innerHTML = "Test Cell 1";
-        cell2.innerHTML = "Test Cell 2";
-        cell3.innerHTML = "Test Cell 3";
+        let currentWeekday = this.weekday
     
-        // let mathRow = document.createElement('tr');
-        // mathContent = document.innertext("");
-        // mathContent = document.getElementById("class");
-        // row = table.insertRow(0)
-
-           
+        const weekday = currentWeekday;
+        
+        let area;
+         
+        switch (weekday) {
+          case 'Monday':
+            area = mondayArea.innerHTML += 
+                `<tr> <td> ${this.subject}</td> <td>${this.content}</td> </tr>`
+            break;
+          case 'Tuesday':
+            area = tuesdayArea.innerHTML += 
+                `<tr> <td> ${this.subject}</td> <td>${this.content}</td> </tr>`
+            break;
+          case 'Wednesday':
+            area = wednesdayArea.innerHTML += 
+                `<tr> <td> ${this.subject}</td> <td>${this.content}</td> </tr>`
+            break;
+        }
     }
-
-
-   
+        
 }
+                
 
 
-    
+
+
+
