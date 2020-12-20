ad615b29b40116950a6135bbd64071d21e7b0b41
anikachristopher <anika.mchristoper@gmail.com>
2020-12-19 20:16:59 -0500

    changed data in the seed folder

--- afterschool_backend/db/seeds.rb
+++ afterschool_backend/db/seeds.rb
@@ -10,12 +10,12 @@ Schedule.delete_all
 
 Child.create(name: "Malik", age: "9", grade: "fourth")
 Schedule.create(weekday: "Monday", subject: "Math", content: "Completed chapter 1 of Go Math", child_id: 1, week: 1)
-Schedule.create(weekday: "Tuesday", subject: "Reading", content: "Completed chapter 1 of Lost", child_id: 1, week: 1)
-Schedule.create(weekday: "Wednesday", subject: "Creative Writing", content: "Wrote a story about my Favorite things to eat", child_id: 1, week: 1)
-Schedule.create(weekday: "Thursday", subject: "Physical Education", content: "Did 15 Jumping Jacks and 15 Sit Ups", child_id: 1, week: 1)
-Schedule.create(weekday: "Friday", subject: "Music", content: "Listened to Beethoven on You Tube", child_id: 1, week: 1)
-Schedule.create(weekday: "Monday", subject: "Math", content: "Completed chapter 2 of Go Math", child_id: 1, week: 2)
-Schedule.create(weekday: "Tuesday", subject: "Reading", content: "Completed chapter 2 of Lost", child_id: 1, week: 2)
-Schedule.create(weekday: "Wednesday", subject: "Creative Writing", content: "Wrote a story about my family", child_id: 1, week: 2)
-
+Schedule.create(weekday: "Monday", subject: "Reading", content: "Completed chapter 1 of Lost", child_id: 1, week: 1)
+Schedule.create(weekday: "Monday", subject: "Creative Writing", content: "Wrote a story about my Favorite things to eat", child_id: 1, week: 1)
+Schedule.create(weekday: "Tuesday", subject: "Math", content: "Did 15 Jumping Jacks and 15 Sit Ups", child_id: 1, week: 1)
+Schedule.create(weekday: "Tuesday", subject: "Reading", content: "Listened to Beethoven on You Tube", child_id: 1, week: 1)
+Schedule.create(weekday: "Tuesday", subject: "Creative Writing", content: "Completed chapter 2 of Go Math", child_id: 1, week: 1)
+Schedule.create(weekday: "Wednesday", subject: "Math", content: "Completed chapter 2 of Lost", child_id: 1, week: 1)
+Schedule.create(weekday: "Wednesday", subject: "Reading", content: "Wrote a story about my family", child_id: 1, week: 1)
+Schedule.create(weekday: "Wednesday", subject: "Creative Writing", content: "Wrote a story about my family", child_id: 1, week: 1)
 
