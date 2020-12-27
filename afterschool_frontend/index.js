16a27846acd2ae046ec67b3aac0115ffe8c9692d
anikachristopher <anika.mchristoper@gmail.com>
2020-12-27 02:28:09 -0500

    append test

--- afterschool_frontend/index.js
+++ afterschool_frontend/index.js
@@ -1,96 +1,102 @@
-document.addEventListener("DOMContentLoaded", () => { 
-    fetchSchedules();
-    addNewSchedule();
-    // selectTables();
-   
+document.addEventListener("DOMContentLoaded", () => {
+  fetchSchedules();
+  addNewSchedule();
+  // selectTables();
+});
 
-})
+const BASE_URL = "http://127.0.0.1:3000";
 
-const BASE_URL = "http://127.0.0.1:3000"
+const submitSchedule = document.querySelector("#create-schedule-button")
 
-function fetchSchedules(){
-    fetch(`${BASE_URL}/schedules`)
-    .then(resp => resp.json())
-    .then(schedules => {
-     schedules["data"].forEach(schedule => { 
+
+function fetchSchedules() {
+  fetch(`${BASE_URL}/schedules`)
+    .then((resp) => resp.json())
+    .then((schedules) => {
+      schedules["data"].forEach((schedule) => {
         //  console.log(schedule);
         //   debugger;
-         let newScheduleCard = 
-         new Schedule(schedule.id, schedule.attributes.weekday, schedule.attributes.subject, schedule.attributes.content, schedule.attributes.child_id);
+        let newScheduleCard = new Schedule(
+          schedule.id,
+          schedule.attributes.weekday,
+          schedule.attributes.subject,
+          schedule.attributes.content,
+          schedule.attributes.child_id
+        );
         //  debugger;
         //  newScheduleCard.createSchedule();
-        newScheduleCard.renderSchedule();    
-        });
-   
+        newScheduleCard.renderSchedule();
+      });
     });
 }
 
+    function createSchedule(){
+      // event.preventDefault();
+    fetch(`${BASE_URL}/schedules`, {
+      method: "POST",
+      headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(schedule),
+  })
+    .then((resp) => resp.json())
+    .then((schedule) => {
+      let s = new Schedule(
+        schedule.id,
+        schedule.weekday,
+        schedule.subject,
+        schedule.content,
+        schedule.child_id
+      );
+      s.renderSchedule();
+    
+    });
+  }
 
-function createSchedule() 
-    {
-      event.preventDefault();
-     // debugger;
-  
-      const schedule = 
-     {
-       weekday: event.target.weekday.value,
-       subject: event.target.subject.value,
-       content: event.target.content.value
-     }
-     fetch(`${BASE_URL}/schedules`,
-     {
-       method: "POST",
-       headers: {
-        Accept: "application/json", "Content-Type": "application/json",
-       },
-       body: JSON.stringify(schedule)
-     })
-     .then(resp => resp.json())
-     .then(schedule => 
-      {
-        let s = new Schedule(schedule.id, schedule.weekday, schedule.subject, schedule.content, schedule.child_id);
-        s.renderSchedule();
-      }) 
-} 
+  submitSchedule.addEventListener('submit', createSchedule);
 
-function deleteSchedule(schedule){
-    // debugger;
-    fetch(`localhost:3000/schedules/${schedule.id}`, {
+function deleteSchedule(schedule) {
+  // debugger;
+  fetch(`localhost:3000/schedules/${schedule.id}`, {
     method: "DELETE",
-    headers: {"Content-Type": "application/json"},
-    })
-    .then(resp => resp.json())
-    .then(schedule => {document.getElementById(`${schedule.id}`.remove())    
-})}
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((resp) => resp.json())
+    .then((schedule) => {
+      document.getElementById(`${schedule.id}`.remove());
+    });
+}
 
-function selectTables(){
-    mon = document.getElementById("monday-delete");
-    tues = document.getElementById("tuesday-delete");
-    wed = document.getElementById("wednesday-delete");
-    thur = document.getElementById("thursday-delete");
-    fri = document.getElementById("friday-delete");
-    mon.addEventListener("click", deleteSchedule);
-    tues.addEventListener("click", deleteSchedule);
-    wed.addEventListener("click", deleteSchedule);
-    thur.addEventListener("click", deleteSchedule);
-    fri.addEventListener("click", deleteSchedule);
-};
+function selectTables() {
+  mon = document.getElementById("monday-delete");
+  tues = document.getElementById("tuesday-delete");
+  wed = document.getElementById("wednesday-delete");
+  thur = document.getElementById("thursday-delete");
+  fri = document.getElementById("friday-delete");
+  mon.addEventListener("click", deleteSchedule);
+  tues.addEventListener("click", deleteSchedule);
+  wed.addEventListener("click", deleteSchedule);
+  thur.addEventListener("click", deleteSchedule);
+  fri.addEventListener("click", deleteSchedule);
+}
 
-
-
-function ShowScheduleModal(){
+function addNewSchedule() {
+  let newScheduleBtn = document.querySelector("#create-schedule"); // identifies the button
+  newScheduleBtn.addEventListener(
+    "click",
+    (event) => ShowScheduleModal(event),
+    false
+  ); // adds event listener to button and redirects to open modal function
+}
+function ShowScheduleModal() {
+  // debugger;
+  //references the modal in html
   const addScheduleModal = document.getElementById("add-modal");
-  const addNewSchedule = document.getElementById("new-schedule-form"); 
-  $(addScheduleModal).modal("show", {backdrop: "static"});
-  
-  addNewSchedule.addEventListener('submit', () => createSchedule(event))
-};
-
-
-function addNewSchedule(){
-  let newScheduleBtn = document.querySelector('#create-schedule');  // identifies the button 
-  newScheduleBtn.addEventListener("click", (event) => showScheduleModal(event), false); // adds event listener to button and redirects to open modal function 
-
-
-};
-
+  // opening the modal
+  $(addScheduleModal).modal("show", {
+    backdrop: "static",
+  });
+  // debugger;
+  addScheduleModal.addEventListener("submit", createSchedule);
+}
--- afterschool_frontend/src/schedule.js
+++ afterschool_frontend/src/schedule.js
@@ -43,5 +43,59 @@ class Schedule{
                   `<tr> <td> ${this.subject}</td> <td>${this.content}</td> </tr>`
               break;
         }
+        
     }
+
+    let newSchedForm = document.getElementById("schedule-form");
+
+  
+    let newWeekValue = document.createElement("value");
+    newWeekValue.innerHTML += `${this.weekday}`;
+    newSchedForm.appendChild(newWeekValue);
+
+
+    let newSubject = document.createElement("value");
+    newSubject.innerHTML += `${this.subject}`;
+    newSchedForm.appendChild(newSubject);
 }
+
+
+
+//     //add review comment to element
+//     reviewBodyElem.innerHTML += `${this.comment}`;
+
+//     //create review comment footer
+//     let reviewFooterElem = document.createElement("footer");
+//     reviewFooterElem.classList.add("blockquote-footer");
+
+//     //add review author to comment footer
+//     reviewFooterElem.innerHTML += `${this.name}`;
+
+//     // Add newly created elements to the DOM
+//     //debugger;
+//     reviewSection.appendChild(reviewDiv);
+
+//     reviewDiv.appendChild(reviewCard);
+
+//     reviewCard.appendChild(headerDiv);
+
+//     reviewCard.appendChild(reviewBodyDiv);
+
+//     reviewBodyDiv.appendChild(reviewBodyBlock);
+
+//     reviewBodyBlock.appendChild(reviewBodyElem);
+
+//     reviewBodyBlock.appendChild(reviewFooterElem);
+
+//     const allReviewsModal = document.getElementById("all-modal");
+//     $(allReviewsModal).modal("show", {
+//       backdrop: "static"
+//     });
+//   }
+
+//   showReviewsModal() {
+//     // fetches reviews and inserts data into modal
+//     renderReviews();
+//     // opens camp reviews modal
+//   }
+// }
